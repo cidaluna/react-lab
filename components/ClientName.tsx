2bd3fd97dc14bab4cf5dc6f15588266d22a829ab
Cida Luna <cidalunna@hotmail.com>
2026-08-19 22:06:05 -0300

    feat: add client name by props with conditional

--- components/ClientName.tsx
+++ components/ClientName.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react"
 
 export const ClientName: FC<{ name: string }> = (props) => {
-  return <p>{props.name}</p>
+  return props.name === 'Cida' ? (<p>Sou a Cida</p>) : (<p>Sou outra pessoa.</p>)
 }
