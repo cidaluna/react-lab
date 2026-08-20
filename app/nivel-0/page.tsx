32c71f035f3811fde4b674e153e031f983247bcb
Cida Luna <cidalunna@hotmail.com>
2026-08-19 22:22:49 -0300

    feat: add client name by props with string, number and formatted Date

--- app/nivel-0/page.tsx
+++ app/nivel-0/page.tsx
@@ -8,7 +8,7 @@ import {ClientName} from '@/components/ClientName';
 
 const Page = () => (
   <div>
-    <ClientName name={'Cida'} age={39} />
+    <ClientName name={'Cida'} age={39} birthDateFake={new Date(1987, 7, 29)} />
   </div>
 );
 
--- components/ClientName.tsx
+++ components/ClientName.tsx
@@ -1,10 +1,10 @@
 import { FC } from "react"
 
-export const ClientName: FC<{ name: string, age:number }> = (props) => {
+export const ClientName: FC<{ name: string, age:number, birthDateFake: Date }> = (props) => {
   return (
     <>
       <p>Sou a {props.name}!</p>
-      <p>Tenho {props.age} anos.</p>
+      <p>Tenho {props.age} anos, porque nasci em {props.birthDateFake.toLocaleDateString('pt-BR')}</p>
     </>
   );
 }
