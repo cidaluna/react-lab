9d623ede26f9079510ae0290bf6af1dcfad8fb21
Cida Luna <cidalunna@hotmail.com>
2026-08-19 22:35:33 -0300

    feat: add client name by props with list html

--- app/nivel-0/page.tsx
+++ app/nivel-0/page.tsx
@@ -5,10 +5,12 @@
 // }
 
 import {ClientName} from '@/components/ClientName';
+import {Hobbies} from '@/components/Hobbies';
 
 const Page = () => (
   <div>
     <ClientName name={'Cida'} age={39} birthDateFake={new Date(1987, 7, 29)} />
+    <Hobbies />
   </div>
 );
 
---
+++ components/Hobbies.tsx
@@ -0,0 +1,10 @@
+export const Hobbies = () => (
+  <>
+    <p>Estes são os meus hobbies:</p>
+    <ul className="list-disc pl-6">
+      <li>Fotografia</li>
+      <li>Natureza</li>
+      <li>Costura</li>
+    </ul>
+  </>
+);
