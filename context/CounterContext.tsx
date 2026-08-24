587574b9c06bd8a3d88c4c7bc4a0b9b07cfd1082
Cida Luna <cidalunna@hotmail.com>
2026-08-24 20:54:49 -0300

    feat: using session storage

--- context/CounterContext.tsx
+++ context/CounterContext.tsx
@@ -1,17 +1,32 @@
 "use client";
 
-import { createContext, Dispatch, ReactNode, SetStateAction, useContext, useState } from 'react';
+import { createContext, Dispatch, ReactNode, SetStateAction, useContext, useEffect, useState } from 'react';
+
+type CounterType = number | null;
 
 type CounterContextType = {
-  counter: number;
-  setCounter: Dispatch<SetStateAction<number>>;
+  counter: CounterType;
+  setCounter: Dispatch<SetStateAction<CounterType>>;
 }
 
-// 1. Inicie como undefined
 export const CounterContext = createContext<CounterContextType | undefined>(undefined);
 
-export default function CounterProvider({children}: { children: ReactNode }) {
-  const [counter, setCounter] = useState(99);
+export default function CounterProvider({ children }: { children: ReactNode }) {
+  const [counter, setCounter] = useState<CounterType>(null);
+
+  useEffect(() => {
+    const counterSessionStorage = sessionStorage.getItem("counter") ?? 0;
+    console.log('counterSessionStorage', counterSessionStorage);
+    if (counterSessionStorage !== null) {
+      setCounter(+counterSessionStorage);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (counter) {
+      sessionStorage.setItem("counter", counter.toString());
+    }
+  }, [counter]);
 
   return (
     <CounterContext.Provider value={{ counter, setCounter }}>
@@ -20,9 +35,11 @@ export default function CounterProvider({children}: { children: ReactNode }) {
   );
 }
 
-// 2. Crie um hook customizado para usar nos componentes
+// 3. Hook customizado para consumo seguro nos componentes da aplicação
 export const useCounter = () => {
   const context = useContext(CounterContext);
-  if (!context) throw new Error(":: useCounter deve ser usado dentro de um CounterProvider");
+  if (!context) {
+    throw new Error(":: useCounter deve ser usado dentro de um CounterProvider");
+  }
   return context;
 };
